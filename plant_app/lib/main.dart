37947ad402fc9f3737d9920b3a02c45c947a21c4
Nguyen Cong Chien <cn98@Nguyens-MacBook-Pro.local>
2021-03-25 22:08:14 +0700

    new project plant app

---
+++ plant_app/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:plant_app/constant.dart';
+import 'package:plant_app/screens/home/components/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          scaffoldBackgroundColor: kBackgroundColor,
+          primaryColor: kPrimaryColor,
+          textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: HomeScreen());
+  }
+}
---
+++ plant_app/lib/screens/home/components/body.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:plant_app/constant.dart';
+
+class Body extends StatelessWidget {
+  const Body({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Column(
+      children: [
+        Container(
+            height: size.height * 0.2,
+            child: Stack(
+              children: [
+                Container(
+                  height: size.height * 0.2 - 27,
+                  decoration: BoxDecoration(
+                      color: kPrimaryColor,
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(36),
+                          bottomRight: Radius.circular(36))),
+                ),
+                Positioned(
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    child: Container(
+                      margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                      height: 54,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                          boxShadow: [
+                            BoxShadow(
+                                offset: Offset(0, 10),
+                                blurRadius: 50,
+                                color: kPrimaryColor.withOpacity(0.23))
+                          ]),
+                    ))
+              ],
+            )),
+      ],
+    );
+  }
+}

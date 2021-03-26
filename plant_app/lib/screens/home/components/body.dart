6fe4392318ab2b0bf1187e8cb9f77be921ce9b76
Nguyen Cong Chien <cn98@Nguyens-MacBook-Pro.local>
2021-03-26 07:54:56 +0700

    push code

--- plant_app/lib/screens/home/components/body.dart
+++ plant_app/lib/screens/home/components/body.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:plant_app/constant.dart';
+import 'header_with_searchbox.dart';
+import 'recomend_plants.dart';
+import 'title_with_more_btn.dart';
 
 class Body extends StatelessWidget {
   const Body({Key key}) : super(key: key);
@@ -7,40 +10,21 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Column(
-      children: [
-        Container(
-            height: size.height * 0.2,
-            child: Stack(
-              children: [
-                Container(
-                  height: size.height * 0.2 - 27,
-                  decoration: BoxDecoration(
-                      color: kPrimaryColor,
-                      borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(36),
-                          bottomRight: Radius.circular(36))),
-                ),
-                Positioned(
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    child: Container(
-                      margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                      height: 54,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(20),
-                          boxShadow: [
-                            BoxShadow(
-                                offset: Offset(0, 10),
-                                blurRadius: 50,
-                                color: kPrimaryColor.withOpacity(0.23))
-                          ]),
-                    ))
-              ],
-            )),
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          HeaderWithSearchBox(size: size),
+          TitleWithMoreBtn(
+            text: "Recomended",
+            press: () {},
+          ),
+          RecomendsPlants(),
+          TitleWithMoreBtn(
+            text: "Recomended",
+            press: () {},
+          ),
+        ],
+      ),
     );
   }
 }

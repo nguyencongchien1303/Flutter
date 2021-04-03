1b1776bea9e87418da0d1b918a9c9312a47f9a10
Nguyen Cong Chien <cn98@Nguyens-MacBook-Pro.local>
2021-04-03 07:58:03 +0700

    removed rác

--- app_sutrix_1/lib 2/validators/validation_login.dart
+++
@@ -1,9 +0,0 @@
-class Validations {
-  static bool isValidPhoneNumber(String phoneNumber) {
-    return phoneNumber != null && phoneNumber.length > 6;
-  }
-
-  static bool isValidPassword(String password) {
-    return password != null && password.length > 6;
-  }
-}
